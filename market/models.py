e9b27f27dff61e8202879594a68112bb62ecba95
ameni neffati <neffatiamani14@gmail.com>
2023-05-16 16:03:59 +0100

    change the budget

--- market/models.py
+++ market/models.py
@@ -12,12 +12,13 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
-    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    budget = db.Column(db.Integer(), nullable=False , default= 5000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
     
     
     @property
     def prettier_budget(self):
+        budget = self.budget or 5000
         if len(str(self.budget)) >= 4:
             return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
         else:
@@ -33,7 +34,12 @@ class User(db.Model, UserMixin):
         
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
-           
+      
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price     
+    
+    def can_sell(self, item_obj):
+        return item_obj in self.items
         
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -44,3 +50,13 @@ class Item(db.Model):
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
+    
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
+        
+    def sell(self, user):
+        self.owner = None
+        user.budget += self.price
+        db.session.commit()
